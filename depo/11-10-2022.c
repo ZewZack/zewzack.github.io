9a83920e4761ea91494df032556b73339c942a56
Enes a.k.a ZewZack <54986847+ZewZack@users.noreply.github.com>
2022-10-11 10:27:41 +0300

    Add files via upload

---
+++ depo/11-10-2022.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+
+// https://github.com/CIleProgramlama/kodlar/blob/main/B%C3%B6l%C3%BCm-07-Diziler/sayfa_82.c
+// yıllık ortalama sıcaklığın en düşük ve en yüksek olan illeri veren programı yaz.
+
+}
